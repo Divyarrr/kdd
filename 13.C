e226075dedcf5f66dd5e71f3378d371cabc3915a
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-03-13 05:24:55 +0530

    Create 13.C

---
+++ 13.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,t,s=0;
+    scanf("%d",&a);
+    t=a;
+    while(a)
+    {
+        t=a%10;
+        s=s+t*t;
+        a=a/10;
+    }
+    printf("%d",s);
+    getch();
+}
