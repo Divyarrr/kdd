a20f9cfd27b773ac5007b5957beae5c3977f5c7f
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-03-13 05:36:33 +0530

    Create 26.C

---
+++ 26.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    char a[100],k;
+    int i;
+    k=' ';
+    scanf("%[^\n]",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==' '&&a[i+1]==' ')
+        {
+            a[i]='$';
+            break;
+        }
+    }
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]!='$')
+        {
+            printf("%c",a[i]);
+        }
+    }
+    getch();
+}
