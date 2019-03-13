9cd35525e5d60b203a7a76a737b3c9e1b9a717c9
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-03-13 05:39:34 +0530

    Create 29.C

---
+++ 29.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,l=0,i,j;
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        for(j=1;j<=i/2;j++)
+        {
+            if(i%j==0&&j*j==i)
+            {
+                l++;
+            }
+        }
+    }
+    printf("%d",l);
+    getch();
+}
