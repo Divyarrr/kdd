5ba10127d2a646787a274ad662f98f590b4d0458
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-03-13 05:35:30 +0530

    Create 24.C

---
+++ 24.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    int i,k=0,l=0;
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        l++;
+    }
+    for(i=0;i<l;i++)
+    {
+        if(a[i]>='0'&&a[i]<='9')
+        k++;
+    }
+    if(k==l)
+    printf("yes");
+    else
+    printf("no");
+    getch();
+}
