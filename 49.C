9a9e24a20958b57b7dc780fd3983a3aa464e9f3c
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-03-13 06:04:23 +0530

    Create 49.C

---
+++ 49.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    long a;
+    scanf("%ld",&a);
+    if(a>=-2,147,483,647&&a<=2,147,483,647)
+    {
+      printf("INT");
+    }
+   if(a>2,147,483,647)
+    {
+         printf("LONG");
+    }
+   
+    getch();
+}
