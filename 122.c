bd4506b9a190cedcb0a9833d4a1809983ce119d0
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-03-26 12:03:09 +0530

    122.c

---
+++ 122.c
@@ -0,0 +1,70 @@
+include<stdio.h>
+void main()
+{
+char a[100];
+int i;
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[3]=='0' && a[4]=='1')
+{
+printf("January");
+break;
+}
+else if(a[3]=='0' &&a[4]=='2')
+{
+printf("February");
+break;
+}
+else if(a[3]=='0' && a[4]=='3')
+{
+printf("March");
+break;
+}
+else if(a[3]=='0' && a[4]=='4')
+{
+printf("April");
+break;
+}
+else if(a[3]=='0'&&a[4]=='5')
+{
+printf("May");
+break;
+}
+else if(a[3]=='0' && a[4]=='6')
+{
+printf("Junje");
+break;
+}
+else if(a[3]=='0'&&a[4]=='7')
+{
+printf("July");
+break;
+}
+else if(a[3]=='0' && a[4]=='8')
+{
+printf("August");
+break;
+}
+else if(a[3]=='0' && a[4]=='9')
+{
+printf("Sepjjtember");
+break;
+}
+else if(a[3]=='1' && a[4]=='0')
+{
+printf("Octoer");
+break;
+}
+else if(a[3]=='1'&&a[4]=='1')
+{
+printf("November");
+break;
+}
+else if(a[3]=='1'&&a[4]=='2')
+{
+printf ("December");
+break;
+}
+}
+}j
