661ac235c6c4265191a80eaf4902e99fdf71d361
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-03-26 14:04:11 +0530

    175.c

---
+++ 175.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main()
+{
+    char a[100],b[100],c[1000]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    int i,j,k=0,c=0,m=0;
+    scanf("%s  ",a);
+    scanf("%s",b);
+    for(i=0;a[i]!='\0';i++);
+    for(j=0;b[j]!='\0';j++);
+    
+    for(k=i;k<i+j;k++)
+    {
+        a[k]=b[m];
+        m++;
+    
+    }
+    a[k]='\0';
+    for(k=0;k<i+j;k++)
+    {
+        for(m=0;c[m]!='\0';m++)
+        {
+            if(a[k]==c[m])
+            {
+                c++;
+                c[m]='&';
+            }
+        }
+    }
+    if(c==26)
+    {
+        printf("co");
+    }
+        else
+        {
+            printf("non cy");
+        }
+    
+    }
